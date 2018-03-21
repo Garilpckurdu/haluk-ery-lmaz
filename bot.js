5d7996e0896c599c8537c43e7a04c7e8c3cf3c05
Garilpckurdu <35172855+Garilpckurdu@users.noreply.github.com>
2018-03-21 17:35:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'hocam') {
-    	message.reply('evet oğlum');
+    	message.reply('evet evlat');
   	}
 });
 
