fa1433a602839db16d77689a0b45baba66007f16
Garilpckurdu <35172855+Garilpckurdu@users.noreply.github.com>
2018-05-17 20:40:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,5 +23,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'FBI') {
+    	message.reply('FBI, OPEN UP!!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
