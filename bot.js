93545ab4df97b370f4c7bb64afc9bfb5f7f3430f
Garilpckurdu <35172855+Garilpckurdu@users.noreply.github.com>
2019-04-29 22:19:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,11 +35,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'sosis') {
-    	message.content('salam');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
