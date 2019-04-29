a34278be4d42aad8de7c07a6a8b52f4db4cbc878
Garilpckurdu <35172855+Garilpckurdu@users.noreply.github.com>
2019-04-29 22:14:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'hocam bir espiri yapsanıza') {
+    	message.reply('Hacca giden dedeye ne denir? Hacim');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
